e768fa37d544ec296aa793ad89e9f430ff8800c4
Pol González Arribas <polgonzarri@gmail.com>
2025-12-04 18:22:37 +0100

    llum-musica-no-error

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
     if (input.lightLevel() > 200) {
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.UntilDone)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
     } else if (input.lightLevel() < 50) {
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.UntilDone)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.InBackground)
     }
 })
